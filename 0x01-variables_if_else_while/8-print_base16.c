d0fa313e7e2bd23a97e77ce0c3b50d5a67fcb58e
sunday4me <sunday4me@gmail.com>
2022-03-12 20:01:44 +0100

    error correct

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,22 +9,6 @@
 
 
 int main(void)
-/*{
-	int i, y;
-	for (i = 0; i < 10; i++)
-	{
-		putchar (i + '0');
-	}
-	for (y = 'a'; y <= 'f'; y++)
-	{
-		putchar (y);
-	}
-
-putchar('\n');
-return (0); 
-} */
-
-
 {
 char base_16;
 
