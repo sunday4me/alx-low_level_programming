1542ac09c34181f150d4d22eecb7f77c9d7894e6
sunday4me <sunday4me@gmail.com>
2022-03-16 17:37:41 +0100

    testing vim

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * _islower - Short description, single line
+ * _islower -Short description, single line
  * @c: contains value to be compared
  * Return: Always 0.
  */
-int _islower(int c)
-
+int_islower(int c)
 {
-
-if (c > 'a' && c < 'z')
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
-
-}
+	if (c > 'a' && c < 'z')
+	{
+		return(1);
+	}
+	else
+	{
+		return (1);
+	}
+}	
+	
