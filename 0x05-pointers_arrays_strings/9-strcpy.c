d216c36e3ccbb43d68c1dec21472f8f4858380f0
sunday4me <31360475+sunday4me@users.noreply.github.com>
2022-03-21 16:59:50 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,7 +10,7 @@ char *strcpy(char *dest, char *src, int n)
 {
 	int i;
 	for (i = 0; src[i] !='\0' && i< n; i++)
-		desr[i] = src[i];
+		dest[i] = src[i];
 	while (i < n) 
 		dest[i++] = '\0';
 	return (dest);
