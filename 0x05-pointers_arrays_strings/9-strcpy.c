b9252db5b2712555de2fafbd57e6e981138d4cc7
sunday4me <sunday4me@gmail.com>
2022-03-21 16:55:52 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _strcpy - copies a string up to n
+ * @dest: Destination of the pointer to the string
+ * @src: Source of the pointer to the string to copy
+ * @n: Number of bytes to copy
+ * Return: Pointer to the destination string.
+ */
+
+char *strcpy(char *dest, char *src, int n)
+{
+	int i;
+	for (i = 0; src[i] !='\0' && i< n; i++)
+		desr[i] = src[i];
+	while (i < n) 
+		dest[i++] = '\0';
+	return (dest);
+}
+
