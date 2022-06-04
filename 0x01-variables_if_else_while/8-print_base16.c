e7f492210b9a1c991ffda83685e6238c254d8f6b
Sunday Jimoh <sunday4me@gmail.com>
2022-06-04 14:01:49 -0700

    test

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,17 +8,14 @@
 
 int main(void)
 {	
- int i, y;	
- 
+int i, y;	
 for (i = 0; i < 10; i++)
 {
 putchar (i+'0');
 }
- 
-for (y = 'a'; y <= 'f'; y++)
-{
+ for (y = 'a'; y <= 'f'; y++)
+ {
 putchar(y); 
-}
+ }
 putchar('\n');
 return(0); 
-} 
