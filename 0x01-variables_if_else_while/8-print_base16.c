cebcb55256be720a6400eaf562f2519bfe2407a0
sunday4me <31360475+sunday4me@users.noreply.github.com>
2022-06-05 17:09:44 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,14 @@ int main(void)
 
 {
 int i, y;	
-for (i = 0; i < 10; i++)
-{
-putchar (i + '0');
-}
-for (y = 'a'; y <= 'f'; y++)
-{
-putchar(y);
-}
-
-putchar('\n');
+   for (i = 0; i < 10; i++)
+   {
+     putchar (i + '0');
+   }
+   for (y = 'a'; y <= 'f'; y++)
+   {
+     putchar(y);
+   }
+     putchar('\n');
 return(0); 
 } 
