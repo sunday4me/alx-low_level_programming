e3e7fd4ebdd8255f0f5adce22083b2150f0c2a43
sunday4me <31360475+sunday4me@users.noreply.github.com>
2022-06-06 14:26:25 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,16 +8,15 @@
 
 int main(void)
 {
-             int num = 0;
-             int letters;
- 
-             while (num < 10)
+  int num = 0;
+  int letters;
+    while (num < 10)
              {
                     putchar(num + '0');
                     num++;
              }
-             for (letters = 'a'; letters <= 'f'; letters++)
+    for (letters = 'a'; letters <= 'f'; letters++)
                     putchar(letters);
-             putchar('\n');
-             return (0);
+                  putchar('\n');
+    return (0);
 }
