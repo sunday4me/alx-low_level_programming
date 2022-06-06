8828a30a57fb285cc0b70e79c8a27b1e89e09356
sunday4me <31360475+sunday4me@users.noreply.github.com>
2022-06-06 14:23:17 +0100

    test ooo

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,23 @@
- #include <stdio.h>
-
+#include <stdio.h>
+#include <string.h>
 /**
- * main - Entry point
- * Return:0
+ *	main - Entry point
+ *
+ *	Return: Always 0 (Success)
  */
+
 int main(void)
 {
-  int i, y;	
-   for (i = 0; i < 10; i++)
-{
-   putchar (i+'0');
-    }
-   for (y = 'a'; y <= 'f'; y++)
-    {
-    putchar(y);
-    }
-   putchar('\n');
-      return(0); 
-} 
+             int num = 0;
+             int letters;
+ 
+             while (num < 10)
+             {
+                    putchar(num + '0');
+                    num++;
+             }
+             for (letters = 'a'; letters <= 'f'; letters++)
+                    putchar(letters);
+             putchar('\n');
+             return (0);
+}
