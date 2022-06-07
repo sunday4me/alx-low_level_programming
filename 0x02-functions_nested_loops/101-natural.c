a9a91d52a276603f36380e004e0ce8bde42bf580
sunday4me <31360475+sunday4me@users.noreply.github.com>
2022-06-07 21:42:51 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,15 +10,16 @@ int main(void)
 {
 	int c = 0;
 	int sum = 0;
-	
+
 	while (c < 1024)
 	{
 		if (c % 3 == 0 || c % 5 == 0)
 		{
 			sum += c;
 		}
-		c++
-		}
+
+		c++;
+	}
 	printf("%i\n", sum);
 	return (0);
 }
