265761623f3dbdfed1f51fb428c7bf2c2cca9100
Sunday Jimoh <sunday4me@gmail.com>
2022-06-07 12:03:03 -0700

    test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ int a = 0, rep, b;
 if (n < 0 || n > 15)
 return;
 
-while (a <= n )
+while (a <= n)
 {
 for (b = 0; b <= n; b++)
 {
@@ -36,7 +36,7 @@ _putchar('0' + rep % 10);
 else
 {
 _putchar('0' + rep / 100);
-_putchar('0' + (rep - 100) / 10 );
+_putchar('0' + (rep - 100) / 10);
 _putchar('0' + rep % 10);
 }
 if (b < n)
